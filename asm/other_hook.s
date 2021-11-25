8a3a146ff412e7b72c5b4c6fb203e76462c879f1
Nathan Wichers <bluroseai@hotmail.com>
2021-11-25 15:54:25 -0500

    unnerf item data files and add all them to
    also remove 60 fps patch
    
    fix the testing purposes

--- asm/other_hook.s
+++ asm/other_hook.s
@@ -182,3 +182,20 @@ bx r2
 .global word_to_store_form_at
 word_to_store_form_at:
 .word 0
+
+//08009A74
+.global CheckCanTakeItem
+CheckCanTakeItem:
+add r0, r5, r1
+ldrh r0, [r0,r3]
+push {r1-r5}
+bl CheckItemByThief
+pop {r1-r5}
+cmp r0, #0x0
+beq backToThief_1
+ldr r0, =0x9A7C + Overlay_12_Start
+bx r0
+
+backToThief_1:
+ldr r0, =0x9A86 + Overlay_12_Start
+bx r0
