03f7b5258b56de1e72c7afb5f83c8a0e7b228d3c
Nathan Wichers <bluroseai@hotmail.com>
2021-12-28 01:31:42 -0500

    add __gnu_thumb1_case_uhi

--- asm/thumb_help.s
+++ asm/thumb_help.s
@@ -57,3 +57,19 @@ __gnu_thumb1_case_sqi:
      pop {r1}
      bx  lr
     .size __gnu_thumb1_case_sqi, . - __gnu_thumb1_case_sqi
+
+.global __gnu_thumb1_case_uhi
+.thumb_func
+.type __gnu_thumb1_case_uhi,function
+__gnu_thumb1_case_uhi:
+    push    {r0, r1}
+    mov     r1, lr
+    lsrs    r1, r1, #1
+    lsls    r0, r0, #1
+    lsls    r1, r1, #1
+    ldrh    r1, [r1, r0]
+    lsls    r1, r1, #1
+    add     lr, lr, r1
+    pop     {r0, r1}
+    bx      lr
+    .size __gnu_thumb1_case_uhi, . - __gnu_thumb1_case_uhi
